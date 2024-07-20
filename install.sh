cfe1b6b1179bcb3b56b016b08a1f5b60a3aaa48f
XDcsy <25985772+XDcsy@users.noreply.github.com>
2024-07-20 22:40:53 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+cd ~
+sudo apt-get update
+sudo apt-get install python3-pip -y
+
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+git clone https://github.com/josStorer/RWKV-Runner
+python3 -m pip install torch torchvision torchaudio
+python3 -m pip install -r RWKV-Runner/backend-python/requirements.txt
+cd RWKV-Runner/frontend
+npm ci
+npm run build
+cd ..
+
+if [ ! -d models ]; then
+    mkdir models
+fi
+
+wget -N https://huggingface.co/BlinkDL/rwkv-6-world/resolve/main/RWKV-x060-World-14B-v2.1-20240719-ctx4096.pth -P models/
+
+sudo apt-get install -y ubuntu-drivers-common
+sudo apt-get install -y nvidia-driver-535
+
+touch ~/install_done
+echo "install success"
