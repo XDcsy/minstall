84f4c463d465c799f9a24d239fcfc7b912fe6dd4
XDcsy <25985772+XDcsy@users.noreply.github.com>
2024-09-05 16:28:00 +0800

    Update vsc.sh

--- vsc.sh
+++ vsc.sh
@@ -1,11 +1,11 @@
+sudo -i
+passwd
+
 curl -O https://raw.githubusercontent.com/coder/code-server/main/install.sh
 sh install.sh
+code-server     # 初始化
 vim ~/.config/code-server/config.yaml
 # 改0.0.0.0:8000，改密码
 sudo systemctl enable --now code-server@$USER
-
-sudo -i
-passwd moomtong
-echo "moomtong ALL=(ALL) ALL" | sudo tee -a /etc/sudoers
 
 sudo ln -s /usr/bin/python3 /usr/bin/python
