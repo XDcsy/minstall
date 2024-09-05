421cf7aa416f210eb9ede75e453da2ff72dd2dcd
XDcsy <25985772+XDcsy@users.noreply.github.com>
2024-09-05 17:16:33 +0800

    Update vsc.sh

--- vsc.sh
+++ vsc.sh
@@ -4,8 +4,12 @@ passwd
 curl -O https://raw.githubusercontent.com/coder/code-server/main/install.sh
 sh install.sh
 code-server     # 初始化
+openssl req -newkey rsa:4096 -nodes -keyout key.pem -x509 -days 365 -out cert.pem
 vim ~/.config/code-server/config.yaml
 # 改0.0.0.0:8000，改密码
+# 加入两行
+# cert: /root/cert.pem
+# cert-key: /root/key.pem          
 sudo systemctl enable --now code-server@$USER
 
 sudo ln -s /usr/bin/python3 /usr/bin/python
