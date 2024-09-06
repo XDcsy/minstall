9a359797a603fcaaaf162888d4f775abece6f837
XDcsy <25985772+XDcsy@users.noreply.github.com>
2024-09-06 10:29:04 +0800

    Update vsc.sh

--- vsc.sh
+++ vsc.sh
@@ -5,10 +5,16 @@ curl -O https://raw.githubusercontent.com/coder/code-server/main/install.sh
 sh install.sh
 code-server     # 初始化
 vim ~/.config/code-server/config.yaml
-# 改0.0.0.0:8000，改密码       
+# 改0.0.0.0:443，改密码，改cert: true       
+sudo setcap cap_net_bind_service=+ep /usr/lib/code-server/lib/node
 sudo systemctl enable --now code-server@$USER
 
+#用firefox访问
+
+
 sudo ln -s /usr/bin/python3 /usr/bin/python
+
+
 
 # 在 CodeServer 中，按 Ctrl + , 打开设置页面搜索 default profile，改成 bash
 bash
