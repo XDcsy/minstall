263e6c62352e5bab2adfbffdd6e51e0f14921d83
XDcsy <25985772+XDcsy@users.noreply.github.com>
2024-12-02 15:10:46 +0800

    Update vsc.sh

--- vsc.sh
+++ vsc.sh
@@ -5,7 +5,14 @@ curl -O https://raw.githubusercontent.com/coder/code-server/main/install.sh
 sh install.sh
 code-server     # 初始化
 vim ~/.config/code-server/config.yaml
+######################################
 # 改0.0.0.0:443，改密码，改cert: true       
+bind-addr: 0.0.0.0:443
+auth: password
+password: let'srapewangyue
+cert: true
+######################################
+
 sudo setcap cap_net_bind_service=+ep /usr/lib/code-server/lib/node
 sudo systemctl enable --now code-server@$USER
 
